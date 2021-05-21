30d74e83715630f89e0aa288cf12552c9e1f2b68
Dan Macko <dmac0516@gmail.com>
2021-05-20 22:01:45 -0400

    Data to Dict, cleanup code

--- FOREX_TD_AmeritradeStrategyTrading.py
+++ FOREX_TD_AmeritradeStrategyTrading.py
@@ -19,24 +19,7 @@ TDSession.login()
 print(TDSession.state['loggedin']) 
 print(TDSession.authstate)
 symbol = TDSession.multiple_symbol_watchlist()
-for Symbol in symbol:
-    for days in range(0,30,1):
-        HistDate = (int(round((datetime.now() - timedelta(days=days)).timestamp())))
-        hist_startDate = str(int(round(((datetime.now() - timedelta(days=days)).timestamp()) * 1000)))
-        hist_symbol = Symbol
-        hist_needExtendedHoursData = True
-        hist_endDate = str(int(round(datetime.now().timestamp() * 1000)))
-        HistDate = (int(round((datetime.now()).timestamp())))
-        HistYear = datetime.fromtimestamp(HistDate).year
-        HistMonth = datetime.fromtimestamp(HistDate).month
-        HistDay = datetime.fromtimestamp(HistDate).day
-        NumbDays = date(HistYear,HistMonth,HistDay).isoweekday()    
-        if NumbDays <= 5:
-            X_DayMA = TDSession.Historical_Endpoint(symbol=hist_symbol, 
-                                                    start_date=hist_startDate,
-                                                    end_date=hist_endDate,
-                                                    extended_hours=hist_needExtendedHoursData
-                                                    )
-        else:
-            False
-
+HistoricalContent = TDSession.Historical_Endpoint(symbol=symbol)
+#Create_Database = TDSession.cursor()
+#Create_Table = TDSession.createTable()
+#importData = TDSession.dataImport_Table()
